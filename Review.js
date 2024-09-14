4622a0ce6323b8b3f35c05d1de2bac6db8b40cef
goutham2507 <121351208+goutham2507@users.noreply.github.com>
2024-09-14 13:04:38 +0530

    Add files via upload

---
+++ Review.js
@@ -0,0 +1,65 @@
+const reviews = [
+    {
+      name: "susan smith",
+      job: "web developer",
+      img:
+        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+      text:
+        "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+    },
+    {
+      name: "anna johnson",
+      job: "web designer",
+      img:
+        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
+      text:
+        "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
+    },
+    {
+      name: "peter jones",
+      job: "intern",
+      img:
+        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+      text:
+        "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
+    },
+    {
+      name: "bill anderson",
+      job: "the boss",
+      img:
+        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+      text:
+        "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
+    },
+  ];
+  const img=document.getElementById('image-change');
+  const author=document.getElementById('a');
+  const job=document.getElementById('b');
+  const info=document.getElementById('c');
+
+  const leftbutton=document.getElementById('e');
+  const rightbutton=document.getElementById('f');
+
+  var current=0;
+
+  function show(person){
+    const item=reviews[person];
+    img.src=item.img;
+    author.textContent=item.name;
+    job.textContent=item.job;
+    info.textContent=item.text;
+  }
+  rightbutton.addEventListener('click', function(){
+    current++;
+    if(current>reviews.length-1){
+        current=0;
+    }
+    show(current);
+  });
+  leftbutton.addEventListener('click', function(){
+    current--;
+    if(current<0){
+        current=reviews.length-1;
+    }
+    show(current);
+  });
